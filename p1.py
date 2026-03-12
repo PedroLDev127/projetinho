9db045df8399e2936733b066786547bf4419f587
Pedro Lucas <ppedrolu008@gmail.com>
2026-03-12 04:08:51 -0300

    first commit

---
+++ p1.py
@@ -0,0 +1,9 @@
+print("calculadora de mc")
+
+a = int(input('digite um numero.:'))
+b = int(input('digite outro  numero.:'))
+
+if a % b == 0:
+    print('e divisivel')
+else:
+    print('nao e divisivel')
